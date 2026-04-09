b6cd729ac3b5fd8e3b707f24440a32a7a219f3c3
Jordy Onrust <code@jonrust.online>
2026-04-09 14:20:22 +0200

    Day 1

---
+++ day1/Adventofcode_20222_Day1.main.kts
@@ -0,0 +1,25 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
+
+val day1Input = File("Day1Input.txt").readText()
+
+fun parseInput(input: String) = input.split("\n\n").map { elf ->
+    elf.lines().map { it.toInt() }
+}
+
+fun List<List<Int>>.topNElves(n: Int): Int {
+    return map { it.sum() }
+        .sortedDescending()
+        .take(n)
+        .sum()
+}
+
+parseInput(day1Input).topNElves(1).println()
+parseInput(day1Input).topNElves(3).println()
